312ef89669b2ca104e771844dec87a33059aab5d
FranDev <cuevabarriosfrancisco@gmail.com>
2024-09-27 16:19:51 -0500

    subiendo revisiones y modificaciones

--- src/main/java/modelo/dao/impl/CrudImpl.java
+++ src/main/java/modelo/dao/impl/CrudImpl.java
@@ -24,7 +24,6 @@ public class CrudImpl implements crud {
 
     public void crear() {
         EntityManager em = emf.createEntityManager();
-
         try {
             em.getTransaction().begin();
             Product product = new Product();
